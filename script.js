75e62495b35498471b008eef646901c952ef095e
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-19 13:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
 
+    let fullName = firstName + " " + lastName;
+    alert(fullName);
 }
+
+document.getElementById('submitbtn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    getFormvalue(); 
+});
