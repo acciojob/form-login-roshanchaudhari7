ede0b2d45369dbe44bee2f39e69d3d6d75b63b53
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-21 15:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
-	let firstName = document.getElementById('firstName').value;
-    let lastName = document.getElementById('lastName').value;
+	let form = document.getElementById("form1");
+	let firstName = form.elements["fname"].value;
+    let lastName = form.elements["lname"].value;
 
     let fullName = firstName + " " + lastName;
     alert(fullName);
